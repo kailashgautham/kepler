be7c4146f991ccead34c4c574577363da5ad8aab
kailashgautham <kailash.gautham@gmail.com>
2025-01-22 20:24:40 +0800

    Initialise spring boot project

---
+++ src/main/java/com/kailashgautham/kepler/HelloController.java
@@ -0,0 +1,13 @@
+package com.kailashgautham.kepler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello Kepler";
+    }
+}
