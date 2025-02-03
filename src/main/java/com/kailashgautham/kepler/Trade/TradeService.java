6ac8f2d895d2631d6ed1d63ce65c41afe1a5216a
kailashgautham <kailash.gautham@gmail.com>
2025-02-03 23:06:09 +0800

    Create trade classes

---
+++ src/main/java/com/kailashgautham/kepler/Trade/TradeService.java
@@ -0,0 +1,11 @@
+package com.kailashgautham.kepler.Trade;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TradeService {
+
+    public boolean makeTrade() {
+        return true;
+    }
+}
