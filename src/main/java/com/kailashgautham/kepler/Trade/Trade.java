08378148ed5d578e5da2883dcc4736d2e7278f8d
kailashgautham <kailash.gautham@gmail.com>
2025-02-04 20:55:42 +0800

    Create entity class for trade

--- src/main/java/com/kailashgautham/kepler/Trade/Trade.java
+++ src/main/java/com/kailashgautham/kepler/Trade/Trade.java
@@ -1,5 +1,25 @@
 package com.kailashgautham.kepler.Trade;
 
+import com.kailashgautham.kepler.enums.OrderType;
+
+import java.util.Date;
+
 public class Trade {
+
+   private Integer id;
+   private String symbol;
+   private Integer quantity;
+   private Double price;
+   private Date date;
+   private OrderType orderType;
+
+   public Trade(Integer id, String symbol, Integer quantity, Double price, Date date, OrderType orderType) {
+      this.id = id;
+      this.symbol = symbol;
+      this.quantity = quantity;
+      this.price = price;
+      this.date = date;
+      this.orderType = orderType;
+   }
 
 }
