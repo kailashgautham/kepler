e81753902a7035a5bc8a0272e8cb0be0a590de65
kailashgautham <kailash.gautham@gmail.com>
2025-02-04 22:13:53 +0800

    Add trade repository and entity code

--- src/main/java/com/kailashgautham/kepler/Trade/TradeController.java
+++ src/main/java/com/kailashgautham/kepler/Trade/TradeController.java
@@ -2,20 +2,26 @@ package com.kailashgautham.kepler.Trade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TradeController {
 
-    private final TradeService tradeService;
+    private final TradeRepository tradeRepository;
 
-    @Autowired
-    public TradeController(TradeService tradeService) {
-        this.tradeService = tradeService;
+    public TradeController(TradeRepository tradeRepository) {
+        this.tradeRepository = tradeRepository;
     }
-    @GetMapping("/trade")
-    public boolean makeTrade() {
-        return tradeService.makeTrade();
+    @GetMapping("/trades")
+    public Iterable<Trade> getAllTrades() {
+        return this.tradeRepository.findAll();
+    }
+
+    @PostMapping("/trades")
+    public Trade addNewTrade(@RequestBody Trade trade) {
+        return this.tradeRepository.save(trade);
     }
 
 }
