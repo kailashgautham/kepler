39feafb2df4d2ebff619ef5335c46cb0a23e4581
kailashgautham <kailash.gautham@gmail.com>
2025-02-06 17:40:30 +0800

    Create recurring function for data pull

--- src/main/java/com/kailashgautham/kepler/KeplerApplication.java
+++ src/main/java/com/kailashgautham/kepler/KeplerApplication.java
@@ -2,14 +2,23 @@ package com.kailashgautham.kepler;
 
 import java.util.Arrays;
 
+import com.kailashgautham.kepler.Data.DataFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class KeplerApplication {
+
+	@Autowired
+	private DataFetcher dataFetcher;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeplerApplication.class, args);
@@ -19,5 +28,10 @@ public class KeplerApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 		};
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void runDataFetcher() {
+		dataFetcher.importMostRecentData();
 	}
 }
